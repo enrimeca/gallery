fc84128fbc04a2a3a587bf8c804e54694a00fa37
Enrique Mejia <enriquemejiacano@gmail.com>
2025-01-14 01:42:08 -0500

    Add image list

---
+++ src/constants/images.ts
@@ -0,0 +1,34 @@
+export const images = [
+  {
+    original: "https://picsum.photos/id/870/1200/900",
+    thumbnail: "https://picsum.photos/id/870/200/15"
+  },
+  {
+    original: "https://picsum.photos/id/1/1200/800",
+    thumbnail: "https://picsum.photos/id/1/300/200"
+  },
+  {
+    original: "https://picsum.photos/id/2/800/800",
+    thumbnail: "https://picsum.photos/id/2/200/200"
+  },
+  {
+    original: "https://picsum.photos/id/3/2400/900",
+    thumbnail: "https://picsum.photos/id/3/400/450"
+  },
+  {
+    original: "https://picsum.photos/id/4/900/500",
+    thumbnail: "https://picsum.photos/id/4/150/200"
+  },
+  {
+    original: "https://picsum.photos/id/5/900/500",
+    thumbnail: "https://picsum.photos/id/5/150/200"
+  },
+  {
+    original: "https://picsum.photos/id/6/900/500",
+    thumbnail: "https://picsum.photos/id/6/150/200"
+  },
+  {
+    original: "https://picsum.photos/id/7/900/500",
+    thumbnail: "https://picsum.photos/id/7/150/200"
+  }
+];
