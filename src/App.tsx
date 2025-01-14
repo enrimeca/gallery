8147080364fa324ee51dbc78d7fe752b8183f765
Enrique Mejia <enriquemejiacano@gmail.com>
2025-01-14 02:14:49 -0500

    Add gallery component to the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Gallery from './components/gallery'
 
 function App() {
 
   return (
     <>
       <h1>Photo Gallery - Enrique Mejía Cano</h1>
+      <Gallery />
     </>
   )
 }
