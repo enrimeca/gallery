637bceda14dce0ed3eb53e5b5988292a3970ad04
Enrique Mejia <enriquemejiacano@gmail.com>
2025-01-14 03:33:55 -0500

    Add active state and arrow buttons

--- src/components/gallery/index.tsx
+++ src/components/gallery/index.tsx
@@ -5,15 +5,24 @@ import "./index.css";
 const Gallery = () => {
   const [ currentImage, setCurrentImage ] = useState(0);
 
-
   const handleClick = (index: number) => {
     setCurrentImage(index);
   }
 
+  const handleNext = () => {
+    setCurrentImage((prev) => (prev + 1) % images.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentImage((prev) => (prev - 1 + images.length) % images.length);
+  };
+
   return (
     <>
       <div className="principal">
+        <button className="arrow" onClick={handlePrev}>&lt;</button>
         <img src={images[currentImage].original} alt="" />
+        <button className="arrow" onClick={handleNext}>&gt;</button>
       </div>
       <div className="thumbnails">
         {
@@ -23,6 +32,7 @@ const Gallery = () => {
                 src={image.thumbnail} 
                 alt="" 
                 onClick={() => handleClick(idx)}
+                className={`thumbnail ${currentImage === idx ? "selected" : ""}`}
               />
             )
           })
